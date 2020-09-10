b37ee38394d3d357ffe2ea338980f7761f22ef60
Ethan Clark <clark.ethan.r@gmail.com>
2020-09-10 17:49:52 -0600

    controllers for billboards, artists, and songs

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ✔️ Create a new rails app and make sure you make it a git repo and have a github repo linked to it
 
-Billboard Model   has_many artists
+✔️Billboard Model   has_many artists
 Artist Model - belongs_to billboard  and has_many songs
-Song Model - belongs_to artist
-Each model has more than one attributes
+✔️Song Model - belongs_to artist
+✔️Each model has more than one attributes
 Have Validations for each model
 All CRUD actions for each Table
-Fill out all the controllers
+✔️Fill out all the controllers
 Fill out the views
 This is going to use nested routes for easier use of controller data flow
 
--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,13 +1,34 @@
 class BillboardsController < ApplicationController
   def index
+    @billboard = Billboard.all
   end
 
   def show
+    @billboard = Billboard.find(params[:id])
   end
 
   def new
+    @billboard = Billboard.new
   end
 
   def edit
+    @billboard = Billboard.find params[:id])
   end
+  def update
+    @billboard = Billboard.find(params[:id])
+    if @billboard.update(billboard_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @billboard = Billboard.find(params[:id])
+    @billboard.destroy
+    redirect_to root_path
+  end
+  private 
+    def billboard_params
+      params.require(:billboard).permit(:name)
+    end
 end
